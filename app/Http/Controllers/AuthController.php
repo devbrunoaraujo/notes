f60d7fc47e000916cc089de701f72e5d9096f292
bparaujo <dev.brunoaraujo@gmail.com>
2025-04-21 16:17:00 -0300

    Criação das migrations users, notes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Stmt\TryCatch;
 
 class AuthController extends Controller
 {
@@ -36,7 +39,19 @@ class AuthController extends Controller
         //get user input
         $username = $request->input('text_username');
         $password = $request->input('text_password');
-        echo'ok';
+        
+
+        //teste database
+
+        try {
+            DB::connection()->getPdo();
+            echo"connection is OK!";
+        } catch (\PDOException $e) {
+            echo"Connection failed".$e->getMessage();
+        }
+
+
+
 
     }
 }
