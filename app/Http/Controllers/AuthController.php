ce23fc5a4e592772b8a2bba7b17c2cbbce6a4499
bparaujo <dev.brunoaraujo@gmail.com>
2025-04-21 20:53:42 -0300

    Autenticacao básica criada

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use PhpParser\Node\Stmt\TryCatch;
@@ -40,18 +40,38 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
         
+        // check user exists
 
-        //teste database
-
-        try {
-            DB::connection()->getPdo();
-            echo"connection is OK!";
-        } catch (\PDOException $e) {
-            echo"Connection failed".$e->getMessage();
+        $user = User::where('username', $username)
+                        ->where('deleted_at', NULL)
+                        ->first();
+        // check user exists! 
+        if(!$user){
+            return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Username ou password incorretos!');
         }
 
+        // check password
+        if(!password_verify($password, $user->password)){
+            return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Username ou password incorretos!');
+        }
+        // update last login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
 
+        //login
+        session([
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username
+            ]
+            ]);
 
-
+        echo 'Usuário '.$user->username.'Autenticado com sucesso!';
     }
 }
