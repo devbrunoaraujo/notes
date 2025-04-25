9a79557415adc3de19d94eb97a1bda87c1f1639b
bparaujo <dev.brunoaraujo@gmail.com>
2025-04-24 21:08:35 -0300

    links e separacao da topbar

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ use Illuminate\Support\Facades\Route;
  });
 
  Route::middleware([CheckIsLogged::class])->group(function(){
-    Route::get('/', [MainController::class, 'index']);
-    Route::get('/newNote', [MainController::class, 'newNote']);
-    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/', [MainController::class, 'index'])->name('home');
+    Route::get('/newNote', [MainController::class, 'newNote'])->name('new');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
  });
 
  
